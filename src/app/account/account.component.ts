0c595ba6f8534f7a199c895ceed23859efd01377
unknown <DarshanBaranwal@MDXN00110374.bku.db.de>
2020-08-01 13:31:24 +0530

    firebase included

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AngularFirestore} from '@angular/fire/firestore';
 @Component({
-  selector: 'app-account',
-  templateUrl: './account.component.html',
-  styleUrls: ['./account.component.scss']
+  selector: "app-account",
+  templateUrl: "./account.component.html",
+  styleUrls: ["./account.component.scss"],
 })
 export class AccountComponent implements OnInit {
+  constructor(private firestore: AngularFirestore) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  registerUser() {
+    this.firestore
+      .collection("user")
+      .snapshotChanges()
+      .subscribe((data) => {
+        console.log(data.map(e => {
+            return {
+              id: e.payload.doc.id,
+              data: e.payload.doc.data()
+            };
+          }
+        ));
+      });
   }
-
 }
